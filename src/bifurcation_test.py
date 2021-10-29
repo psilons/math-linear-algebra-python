7a0930db1b15c0ec3b00b8dc68ec24ef01ea3f02
psilons <cuirick@gmail.com>
2021-10-28 21:53:45 -0400

    updates

---
+++ src/bifurcation_test.py
@@ -0,0 +1,3 @@
+# https://isquared.digital/visualizations/2020-11-18-bufurcation-diagram/
+# https://github.com/IlievskiV/Amusive-Blogging-N-Coding/blob/master/Visualizations/bifurcation_diagram.ipynb
+# Fractal
